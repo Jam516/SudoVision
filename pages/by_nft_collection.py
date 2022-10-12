4ac2fec2bb042edb37954d8d95798aced149de38
Jam516 <kufuorkofi@gmail.com>
2022-10-12 16:23:36 +0100

    Add credit link

--- pages/by_nft_collection.py
+++ pages/by_nft_collection.py
@@ -122,7 +122,7 @@ st.set_page_config(
 )
 
 st.title('SudoSwap Pool Analysis')
-st.markdown("[By Kofi](###https://twitter.com/0xKofi)")
+st.markdown("[By Kofi](https://twitter.com/0xKofi)")
 
 'Enter the NFT contract address'
 
